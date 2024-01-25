7eea55f5129ea6608ca5af2790dd906decf64e26
Mark Fehrenbacher <kendopunk@hotmail.com>
2024-01-25 10:51:14 -0500

    Finishing up Session 2 (#1)
    Finishing up Session 2

---
+++ session2/src/HOMEWORK.md
@@ -0,0 +1,41 @@
+# Session 1 Homework
+
+## 1) Working with cargo, crates.io and Dependencies
+
+1. We're building a simple Rust program that needs to convert between camel (HelloWorld), kebab (hello-world) and snake (hello_world) cases.
+1. Create a new `cargo` project and name it whatever you'd like.
+1. Using [crates.io](https://crates.io/), see if you can find a package that does this kind of case conversion (it does exist).
+1. Follow the instructions on crates.io to add this dependency to your project
+1. Write a program which converts "hello world":
+   1. From snake to camel
+   1. From snake to kebab
+   1. from kebab to camel
+   1. from kebab to snake
+   1. from camel to snake
+   1. from came to kebab
+1. Suggest using the `assert_eq!` macro rather than `println!`
+
+```rust
+fn main() {
+  assert_eq!("camelCase", your_snake_to_camel_function("camel_case"));
+}
+```
+
+## 2) Fix main.rs in session2
+
+1. session2 `main.rs` has several intentional errors
+1. Remove the block comments from the words `UNCOMMENT HERE...` to the words `...TO HERE`
+1. ...or pull out blocks of code individually outside the comments and work on them one at a time.
+1. Run `$> cargo run --bin session2` and reviewing the errors and warnings
+1. Fix the problems using the Rust Analyzer extension and the compiler output
+
+---
+
+## 3) Fix test.rs in session2
+
+1. `test.rs` also has intentional errors as well
+1. Remove the block comments from the words `UNCOMMENT HERE...` to the words `...TO HERE`
+1. Using `$> cargo test --bin session2`
+1. Using what you learned in the second lesson, fix the test code.
+
+---
---
+++ session2/src/main.rs
@@ -0,0 +1,98 @@
+mod test;
+
+#[allow(dead_code)]
+fn multiply_two_numbers(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[allow(dead_code)]
+fn square_float(a: f32) -> f32 {
+    a * a
+}
+
+#[allow(dead_code)]
+fn is_odd(num: i32) -> bool {
+    num % 2 != 0
+}
+
+fn main() {
+    println!("Hello, Session 2");
+    /*
+    UNCOMMENT HERE...
+
+    // println!
+    let my_tuple = (100, 33.3, 'x');
+    println!("my_tuple = {}", my_tuple);
+
+    // FUNCTIONS
+    let result = multiply_two_numbers(10i32, 20u32);
+    assert_eq!(result, 200);
+
+    // SCOPING
+    let a: u8 = 10;
+    println!("a = {}", a);
+    {
+        let b: u8 = 20;
+        println!("b = {}", b);
+    }
+    println!("a is {} and b is {}", a, b);
+
+    // CONTROL FLOW
+    let num: i32 = 200;
+    if (num <= 100) {
+        println!("LTE 100");
+    } elif num <= 200 {
+        println!("LTE 200");
+    } else {
+        println!("GTE 200");
+    }
+
+    // LET-IF
+    let passed = true;
+    let score = if passed { 60 } else { "fail" };
+    println!("score = {}", score);
+
+    // LOOPS
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if (counter == 25) {
+            exit;
+        }
+    };
+    println!("result = {}", result);
+
+    // PRINT THE SQUARE OF ALL VALUES OF AN ARRAY
+    let arr: [f32; 5] = [1.1, 2.2, 3.3, 4.4, 5.5];
+    foreach element in arr {
+        println!("{}", square_float(element));
+    }
+
+    // WHAT IS A BETTER WAY TO WRITE THIS MATCH?
+    let apples: u8 = 7;
+    match apples {
+        1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => println!("You have less than 10 apples."),
+        _x if apples >= 10 => println!("You have 10 or more apples."),
+        _ => println!("I don't know how many apples you have.")
+    };
+
+    // PATTERN MATCHING
+    let x = 7.5;
+    let number_is_odd = match x {
+        _x if is_odd(x) => true,
+        _x if !is_odd(x) => false
+    };
+    println!("Number is odd? {}", number_is_odd);
+
+    // Using the binding syntax (@), make this code print "At least one passing grade."
+    // passing grade = 60 or better
+    let scores: (u16, u16, u16) = (55, 58, 72);
+    match scores {
+        [[[YOUR_CODE_HERE_USING_BINDINGS]]] => println!("At least one passing grade."),
+        _ => println!("Everyone failed."),
+    }
+
+    ...TO HERE
+    */
+}
