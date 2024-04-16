03bd12e9ab4bb8b96337043f6e3566c753da5b18
Mark Fehrenbacher <kendopunk@hotmail.com>
2024-04-16 15:21:37 -0400

    cleaning up test code

--- polars_sandbox/src/bin/polars_playground.rs
+++ polars_sandbox/src/bin/polars_playground.rs
@@ -3,30 +3,10 @@
  * cargo run --bin polars_playground
  ****************************************/
 #![allow(dead_code, unused_variables, unused_imports)]
-
 use polars::prelude::*;
-use polars_ops::pivot::*;
 
 fn main() -> Result<(), PolarsError> {
-    let df = df! [
-        "Fruit" => ["apple", "pear", "orange", "apple"],
-        "Color" => ["red", "green", "orange", "golden"],
-        "Round" => ["fred", "marge", "chad", "wopper"],
-        "PricePerPound" => [1.25, 1.39, 1.79, 1.17],
-        "Count" => [20, 10, 7, 15]
-    ]
-    .unwrap();
+    println!("Hello Polars Playground!");
 
-    let df_pivot = pivot_stable(
-        &df,
-        ["Color"],
-        ["Fruit"],
-        Some(["Count", "PricePerPound"]),
-        true,
-        Some(PivotAgg::Sum),
-        Some("->"),
-    )?;
-
-    println!("{}", df_pivot);
     Ok(())
 }
